02ad20d35d38991cec91349faaab053fc1b96ae7
Naing Win <dbnightactor@gmail.com>
2015-08-01 23:33:38 +0630

    change name

--- resources/views/admin/template/partials/header.blade.php
+++ resources/views/admin/template/partials/header.blade.php
@@ -1,7 +1,7 @@
 <!-- header logo: style can be found in header.less -->
 <header class="header">
 	<a href="index.html" class="logo">
-		Poutkaya
+		MM Flood Data
 	</a>
 	<!-- Header Navbar: style can be found in header.less -->
 	<nav class="navbar navbar-static-top" role="navigation">
