94801b3a0b16e1d1d827cff712af4e76a7cc3245
Naing Win <dbnightactor@gmail.com>
2015-08-14 13:29:09 +0630

    ngo add

---
+++ resources/views/admin/theme/partials/form-script.blade.php
@@ -0,0 +1,13 @@
+{!! Html::script('assets/admin-theme/fuelux/js/spinner.min.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-fileupload/bootstrap-fileupload.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-wysihtml5/wysihtml5-0.3.0.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-wysihtml5/bootstrap-wysihtml5.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-datepicker/js/bootstrap-datepicker.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-daterangepicker/moment.min.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-daterangepicker/daterangepicker.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-colorpicker/js/bootstrap-colorpicker.js') !!}
+{!! Html::script('assets/admin-theme/bootstrap-timepicker/js/bootstrap-timepicker.js') !!}
+{!! Html::script('assets/admin-theme/jquery-multi-select/js/jquery.multi-select.js') !!}
+{!! Html::script('assets/admin-theme/jquery-multi-select/js/jquery.quicksearch.js') !!}
+{!! Html::script('assets/admin-theme/advanced-form-components.js') !!}
