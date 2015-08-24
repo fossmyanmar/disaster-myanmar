06116965148742fc993f186bccdc44f9d2760804
Naing Win <dbnightactor@gmail.com>
2015-08-24 23:26:33 +0630

    frontend index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,72 @@
 @extends('frontend.theme.layout')
 
 @section('content')
-<p class="lead text-center">Home</p>
+<div class="row">
+	<div class="col-md-7">
+		<h3><i class="fa fa-file-text-o"></i> Latest News</h3>
+		<hr>
+		<div class="blog-item">
+			<h4><a href="blog_detail.html">Suspendisse dignissim in sem eget pulvinar. Mauris. </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,</p>
+			<a href="blog_detail.html" class="btn btn-sm btn-default">Continue Reading</a>
+		</div>
+		<div class="blog-item">
+			<h4><a href="blog_detail.html">Suspendisse dignissim in sem eget pulvinar. Mauris. </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,</p>
+			<a href="blog_detail.html" class="btn btn-sm btn-default">Continue Reading</a>
+		</div>
+		<div class="blog-item">
+			<h4><a href="blog_detail.html">Suspendisse dignissim in sem eget pulvinar. Mauris. </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,</p>
+			<a href="blog_detail.html" class="btn btn-sm btn-default">Continue Reading</a>
+		</div>
+	</div>
+	<div class="col-md-5">
+		<h3><i class="fa fa-bullhorn"></i> Alert</h3>
+		<div class="about-testimonial boxed-style about-flexslider ">
+			<section class="slider">
+				<div class="flexslider">
+					<ul class="slides about-flex-slides">
+						<li>
+							<a class="about-testimonial-author" href="#">Ericson Reagan</a>
+							<span class="about-testimonial-company">ABC Realestate LLC</span>
+							<div class="about-testimonial-content">
+								<p class="about-testimonial-quote">
+									Pellentesque et pulvinar enim. Quisque at tempor ligula. Maecenas augue ante, euismod vitae egestas sit amet, accumsan eu nulla. Nullam tempor lectus a ligula lobortis pretium. Donec ut purus sed tortor malesuada venenatis eget eget lorem.
+								</p>
+							</div>
+						</li>
+						<li>
+							{{-- <div class="about-testimonial-image ">
+								<img alt="" src="img/avatar2.jpg">
+							</div> --}}
+							<a class="about-testimonial-author" href="#">Jonathan Smith</a>
+							<span class="about-testimonial-company">DEF LLC</span>
+							<div class="about-testimonial-content">
+								<p class="about-testimonial-quote">
+									Pellentesque et pulvinar enim. Quisque at tempor ligula. Maecenas augue ante, euismod vitae egestas sit amet, accumsan eu nulla. Nullam tempor lectus a ligula lobortis pretium. Donec ut purus sed tortor malesuada venenatis eget eget lorem.
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+		
+		<h3><i class="fa fa-microphone"></i> Help Needed</h3>
+		<hr>
+		<div>
+			<h4><a href="#">Suspendisse dignissim in sem </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets..</p>
+		</div>
+		<div>
+			<h4><a href="#">Suspendisse dignissim in sem </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets..</p>
+		</div>
+		<div>
+			<h4><a href="#">Suspendisse dignissim in sem </a></h4>
+			<p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets..</p>
+		</div>
+	</div>
+</div>
 @stop
